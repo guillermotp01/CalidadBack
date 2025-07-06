8de5f11ba08afa2b8557aee2f618dfc5070a5529
Guillermo Trujillo Pantoja <gtrujillo3@yobelscm.biz>
2025-07-06 00:56:20 -0500

    ruta para el retorno a pagina cuando el pago es correcto

--- src/main/java/com/proyecto/calidad/services/MercadoPagoService.java
+++ src/main/java/com/proyecto/calidad/services/MercadoPagoService.java
@@ -51,7 +51,7 @@ public class MercadoPagoService {
 
             // Configurar las URLs de retorno
             PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest.builder()
-                    .success("https://localhost:4200/success")  // cambiar la url pero no puede ser local
+                    .success("https://ecommerce-pi-five.vercel.app/misCompras/success")  // cambiar la url pero no puede ser local
                     .pending("https://f486-38-253-146-176.ngrok-free.app/mis-viajes/pending")
                     .failure("https://f486-38-253-146-176.ngrok-free.app/mis-viajes/failure")
                     .build();
