db6494d60539d9542a4fd6742aa1a378dd2770c9
Guillermo Trujillo Pantoja <gtrujillo3@yobelscm.biz>
2025-07-06 04:25:25 -0500

    cambiando la ruta a carrito

--- src/main/java/com/proyecto/calidad/services/MercadoPagoService.java
+++ src/main/java/com/proyecto/calidad/services/MercadoPagoService.java
@@ -51,9 +51,9 @@ public class MercadoPagoService {
 
             // Configurar las URLs de retorno
             PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest.builder()
-                    .success("https://ecommerce-pi-five.vercel.app/misCompras/success")  // cambiar la url pero no puede ser local
-                    .pending("https://ecommerce-pi-five.vercel.app/misCompras/pending")
-                    .failure("https://ecommerce-pi-five.vercel.app/misCompras/failure")
+                    .success("https://ecommerce-pi-five.vercel.app/carrito/success")  // cambiar la url pero no puede ser local
+                    .pending("https://ecommerce-pi-five.vercel.app/carrito/pending")
+                    .failure("https://ecommerce-pi-five.vercel.app/carrito/failure")
                     .build();
 
             // Crear la preferencia
